2528a4297b6b9c49a1bf3c228a466da88354dfdd
Nemanja Mirkovic <nemanja.nm123@gmail.com>
2025-09-21 15:43:53 +0200

    Implementiano obradjivanje korisnickih komandi

--- SpaceInvaders/SpaceInvaders.Server/Program.cs
+++ SpaceInvaders/SpaceInvaders.Server/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using SpaceInvaders.Common.Game;
+using SpaceInvaders.Server.UI;
+using Spectre.Console;
+
+namespace SpaceInvaders.Server
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var gameConfiguration = new GameConfiguration(40, 20, 30, GameMode.SinglePlayer, 30, 1, 2);
+            var server = new GameServer(gameConfiguration);
+
+            try
+            {
+                server.Start();
+            }
+            catch (Exception ex)
+            {
+                AnsiConsole.Clear();
+                ServerUi.ShowError(ex.Message);
+            }
+        }
+    }
+}
